768782337061fa133744e1525603a2b8a0692f35
Danny12244 <pradeep.py725@gmail.com>
2022-12-03 01:08:50 +0530

    small fix

--- cointab-backend/controller/userController.js
+++ cointab-backend/controller/userController.js
@@ -1,22 +1,25 @@
-const { Router } = require('express');
-const bcrypt = require('bcrypt');
-const userModel = require('../model/user.model');
+const { Router } = require("express");
+const bcrypt = require("bcrypt");
+const userModel = require("../model/user.model");
 const userController = Router();
 
-userController.post('/user', async (req, res) => {
-    const { name, email, password, phone } = req.body;
+userController.post("/user", async (req, res) => {
+  const { name, email, password, phone } = req.body;
 
-    bcrypt.hash(password, 8, async function (err, hash) {
-        if (err) {
-          return res.send("singup failed");
-        }
-    
-        const new_user = new userModel({ email, name, password: hash, phone});
-    
-        await new_user.save().then((res)=>console.log(res)).catch(err=>res.send('user already exists'));
-    
-        res.send("signup successfull");
-      });
-})
+  bcrypt.hash(password, 8, async function (err, hash) {
+    if (err) {
+      return res.send("singup failed");
+    }
+
+    const new_user = new userModel({ email, name, password: hash, phone });
+
+    await new_user
+      .save()
+      .then(() =>  res.send("signup successfull"))
+      .catch(() => res.send("user already exists"));
+
+   
+  });
+});
 
 module.exports = userController;
