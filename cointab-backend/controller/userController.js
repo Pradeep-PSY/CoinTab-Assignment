0976d45df04ba2d4511b25774b74ecd0ae589664
Danny12244 <pradeep.py725@gmail.com>
2022-12-05 23:55:29 +0530

    small improvement

--- cointab-backend/controller/userController.js
+++ cointab-backend/controller/userController.js
@@ -110,20 +110,20 @@ userController.post('/login', async (req, res) => {
 })
 
 userController.get('/logout', async (req, res) =>{
-  return res.send({message: "Logout Successfully", isAuth: false})
-  //  let x = req.session.destroy(function(err) {
-  //       if(err) {
-  //         return false
-  //       }
-  //       else{
-  //         return true
-  //       }
-  //     })
+  // return res.send({message: "Logout Successfully", isAuth: false})
+   let x = req.session.destroy(function(err) {
+        if(err) {
+          return false
+        }
+        else{
+          return true
+        }
+      })
 
-  //     if(x == false){
-  //       return  res.send({message: "Logout Successfully", isAuth: false})
-  //     }
-  //   else res.send({message: "Logout Successfully", isAuth: false})
+      if(x == false){
+        return  res.send({message: "Logout Successfully", isAuth: false})
+      }
+    else res.send({message: "Logout Successfully", isAuth: false})
 
 })
 
